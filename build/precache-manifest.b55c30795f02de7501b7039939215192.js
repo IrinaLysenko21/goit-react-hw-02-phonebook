d2b512c98f7a6335f061760a4391ba1709bab25f
Irina Lysenko <irina.lysenko21@gmail.com>
2019-12-21 11:13:24 +0200

    build folder removed from .gitignore

---
+++ build/precache-manifest.b55c30795f02de7501b7039939215192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f9c6b6e0e517b272af9047842bc79a",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "579062e098b3d9a805b9",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.34041091.chunk.css"
+  },
+  {
+    "revision": "162df7c8b872b0a87ef9",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.cfcd1944.chunk.css"
+  },
+  {
+    "revision": "579062e098b3d9a805b9",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.f649a9d1.chunk.js"
+  },
+  {
+    "revision": "162df7c8b872b0a87ef9",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.61e05469.chunk.js"
+  },
+  {
+    "revision": "530ecffa164a85a05a40",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.106b7e5a.js"
+  }
+]);
