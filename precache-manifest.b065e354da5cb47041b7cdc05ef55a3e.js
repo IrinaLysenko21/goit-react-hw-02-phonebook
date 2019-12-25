e3aed7b091c498ebe012cd77e5a6e4296d0268e3
Irina Lysenko <irina.lysenko21@gmail.com>
2019-12-25 12:52:10 +0200

    Updates

--- precache-manifest.b065e354da5cb47041b7cdc05ef55a3e.js
+++ precache-manifest.b065e354da5cb47041b7cdc05ef55a3e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5f9c6b6e0e517b272af9047842bc79a",
+    "revision": "9c0baad2c38b1806860afbb3af4dd74a",
     "url": "/goit-react-hw-02-phonebook/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-phonebook/static/css/2.34041091.chunk.css"
   },
   {
-    "revision": "162df7c8b872b0a87ef9",
-    "url": "/goit-react-hw-02-phonebook/static/css/main.cfcd1944.chunk.css"
+    "revision": "9a07220338591473598c",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.81630469.chunk.css"
   },
   {
     "revision": "579062e098b3d9a805b9",
     "url": "/goit-react-hw-02-phonebook/static/js/2.f649a9d1.chunk.js"
   },
   {
-    "revision": "162df7c8b872b0a87ef9",
-    "url": "/goit-react-hw-02-phonebook/static/js/main.61e05469.chunk.js"
+    "revision": "9a07220338591473598c",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.6965de34.chunk.js"
   },
   {
     "revision": "530ecffa164a85a05a40",
